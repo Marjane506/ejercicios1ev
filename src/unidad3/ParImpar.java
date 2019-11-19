527a60adff01c6cbe29c77c0ceacb0233f6d079d
Marjane <marjane_rodrigues@hotmail.com>
2019-11-19 17:34:54 +0100

    Ejercicio 1 resuelto.

---
+++ src/unidad3/ParImpar.java
@@ -0,0 +1,28 @@
+package unidad3;
+
+import java.util.Scanner;
+
+public class ParImpar {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("¿Par o Impar?");
+		
+		int num = in.nextInt();
+		
+
+		if (num % 2 == 0) {
+
+			System.out.println(num + " es un número Par.");
+		}
+
+		else {
+
+			System.out.println(num + " es un número Impar");
+
+			in.close();
+		}
+
+	}
+
+}
